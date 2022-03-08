6f9d5ef8a2c09b93c34d48628282be4a7d5c1b12
t11915jr <jeremy.roe@student.manchester.ac.uk>
2022-03-08 13:20:02 +0000

    rewrote code to be in a class

--- lab1/imclient.py
+++ lab1/imclient.py
@@ -1,11 +1,10 @@
 import sys
-
 import im
 import time
 
 
 # problem with server['conn']
-class colors:
+class colours:
     HEADER = '\033[95m'
     BLUE = '\033[94m'
     CYAN = '\033[96m'
@@ -17,73 +16,129 @@ class colors:
     UNDERLINE = '\033[4m'
 
 
-def end_connection(message="connection to server ended."):
-    print(message)
-    if len(server.keys()) <= 2:
-        print("cleared")
-        server.clear()
-    else:
-        server.clear()
-        print("not clear")
-        server['conn'] = 'end'
-    sys.exit()
+def decode(m):
+    return m.decode().strip()
 
 
-try:
-    server = im.IMServerProxy('https://web.cs.manchester.ac.uk/t11915jr/COMP28112/lab1/IMserver.php')
-    # print(len(server.keys()))
-    if len(server.keys()) == 1:
-        server['user1'] = 'set'
-        server['turn'] = '1'
-        server['message'] = ''
-        my_id = 1
-    elif len(server.keys()) == 4:
-        server['user2'] = 'set'
-        my_id = 2
-    else:
-        print('Server is full. Try again later')
-        sys.exit()
+def encode(m):
+    return m.encode('UTF-8')
 
-    my_turn = bool(my_id)
 
-    loading = ""
-    while len(server.keys()) == 4:
-        print(f'Searching for second client {loading}', end='\r')
-        if loading.count(".") <= 10:
-            loading += "."
+class Server:
+
+    def __init__(self):
+        self.turn = None
+        self.user = None
+        self.message = ''
+        try:
+            self.server = im.IMServerProxy('https://web.cs.manchester.ac.uk/t11915jr/COMP28112/lab1/IMserver.php')
+        except ConnectionRefusedError:
+            print("Cannot connect to server")
+            sys.exit(1)
+
+    def connect(self):
+        try:
+            if len(self.server.keys()) == 1:
+                self.server['conn'] = '0'
+            if decode(self.server['conn']) == '0':
+                self.turn = True
+                self.server['conn'] = encode('1')
+            elif decode(self.server['conn']) == '1':
+                self.turn = False
+                self.server['conn'] = encode('connected')
+            elif decode(self.server['conn']) == 'connected':
+                print("Server is currently in use. Try again later")
+                sys.exit()
+            else:
+                self.close_connection("error with server. Try again")
+
+            loading = ""
+            while decode(self.server['conn']) != 'connected':
+                print(f"Searching for second user {loading}", end='\r')
+                loading += '.'
+                if loading.count('.') >= 10:
+                    self.close_connection("timeout. Second user not found")
+                time.sleep(1)
+
+            print(f'connected. Welcome to the Messaging System for Healthcare Professionals.\n')
+            print('-------------------------------------------------------\n')
+            self.server['received'] = encode('0')
+            self.server['message'] = encode('')
+
+        except KeyboardInterrupt:
+            self.close_connection("Connection interrupted by keyboard. Closing connection to server", 1)
+
+    def start_im_service(self):
+        try:
+            while self.check_connection():
+                if self.turn:
+                    self.send_message()
+                else:
+                    self.await_message()
+            self.close_connection("Other user left the conversation. " +
+                                  "Thank you for using the Messaging System for Healthcare Professionals.")
+        except KeyboardInterrupt:
+            self.close_connection("Connection interrupted by keyboard. Closing connection to server", 1)
+
+    def send_message(self):
+        myMessage = input("Enter a message to send to the other user: ")
+
+        if self.check_connection():
+            if myMessage == '\n' or myMessage == '':
+                print(f'{colours.WARNING}Error. No message to send...{colours.ENDC}')
+                return
+            self.server['message'] = encode(myMessage)
+            loading = ""
+            while decode(self.server['received']) == '0':
+                print(f"message sending{loading}", end='\r')
+                if loading.count('.') >= 10:
+                    self.close_connection("timeout. Message could not be sent")
+                time.sleep(1)
+                loading += '.'
+            print("\nmessage sent")
+            self.server['received'] = encode('0')
+            self.next_turn()
+
+    def await_message(self):
+        while decode(self.server['message']) == '':
+            if self.check_connection():
+                print("Other user is typing", end='\r')
+                time.sleep(1)
+
+        if self.check_connection():
+            print("You have received a message:\n" +
+                  f"{colours.CYAN}{self.server['message'].decode().strip()}{colours.ENDC}")
+            self.server['received'] = encode('1')
+            self.server['message'] = encode('')
+            self.next_turn()
             time.sleep(1)
-        else:
-            print('timeout. Closing connection to server')
-            server.clear()
+        print("\n")
+
+    def next_turn(self):
+        self.turn = not self.turn
+
+    def check_connection(self):
+        return decode(self.server['conn']) == 'connected'
+
+    def close_connection(self, m='connection to server closed', e=0):
+        if decode(self.server['conn']) == 'connected':
+            self.server.clear()
+            self.server['conn'] = encode('closing')
+            print('Closing connection with server')
             sys.exit()
 
-    print(f'connected. Welcome to the Messaging System for Healthcare Professionals. You are user {my_id}\n')
-    print('-------------------------------------------------------\n')
-    received_message = ''
-    server['conn'] = 'started'
-    while server['conn'].decode().strip() != 'end':
-        if str(my_id) in server['turn'].decode():
-            myMessage = input("\nEnter a message to send to the other user: ")
-            if myMessage == '\n' or myMessage == '':
-                print(f'{colors.WARNING}Error. No message to send...{colors.ENDC}')
-            elif len(server.keys()) == 2 or len(server.keys()) == 1:
-                end_connection()
-            else:
-                server['message'] = myMessage.encode('UTF-8')
-                if my_id == 1:
-                    server['turn'] = '2'.encode('UTF-8')
-                else:
-                    server['turn'] = '1'.encode('UTF-8')
-                print(f'{colors.BOLD}message sent{colors.ENDC}\n')
-                time.sleep(0.1)
-        else:
-            print(f"{colors.UNDERLINE}Other user is typing...{colors.ENDC}", end='\r')
-            time.sleep(1)
-            if server['message'].decode() != '\n' and server['conn'].decode().strip() == 'started':
-                print(f"{colors.BLUE}You have received a message.\n" +
-                      f"{colors.CYAN}{server['message'].decode().strip()}{colors.ENDC}")
-                server['message'] = ''
+        if decode(self.server['conn']) == 'closing':
+            self.server.clear()
+            print('Closing connection with server')
+            sys.exit()
 
-    end_connection()
-except KeyboardInterrupt:
-    end_connection()
+        else:
+            print(m)
+            self.server.clear()
+            self.server['conn'] = encode('0')
+            sys.exit(e)
+
+
+s = Server()
+s.connect()
+s.start_im_service()
