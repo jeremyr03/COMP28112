fbefe648c2ea0c57c768190366c8f463bb8b78c8
t11915jr <jeremy.roe@student.manchester.ac.uk>
2022-03-09 13:20:04 +0000

    edit code

--- lab1/imclient.py
+++ lab1/imclient.py
@@ -1,6 +1,7 @@
 import sys
 import im
 import time
+import urllib.error
 
 
 # problem with server['conn']
@@ -28,18 +29,25 @@ class Server:
 
     def __init__(self):
         self.turn = None
-        self.user = None
+        # self.user = None
         self.message = ''
         try:
             self.server = im.IMServerProxy('https://web.cs.manchester.ac.uk/t11915jr/COMP28112/lab1/IMserver.php')
-        except ConnectionRefusedError:
-            print("Cannot connect to server")
+        except urllib.error:
+            print("Cannot connect to server :( \nAre you connected to Eduroam/GlobalConnect VPN???")
             sys.exit(1)
 
     def connect(self):
+        # server['conn'] is used to let the clients know what is happening:
+        # 0 = no one connected; 1 = one person connected; connected = connection established, start mesaging service
+        # closing = server is being shut down
         try:
             if len(self.server.keys()) == 1:
                 self.server['conn'] = '0'
+            elif decode(self.server['conn']) == 'closing':
+                self.server.clear()
+                self.server['conn'] = '0'
+
             if decode(self.server['conn']) == '0':
                 self.turn = True
                 self.server['conn'] = encode('1')
@@ -57,26 +65,34 @@ class Server:
                 print(f"Searching for second user {loading}", end='\r')
                 loading += '.'
                 if loading.count('.') >= 10:
-                    self.close_connection("timeout. Second user not found")
+                    self.close_connection("timeout. Second user not found. Closing connection to server.")
                 time.sleep(1)
 
             print(f'connected. Welcome to the Messaging System for Healthcare Professionals.\n')
             print('-------------------------------------------------------\n')
+            # server['received'] used to determine whether the other user has received the message or not
             self.server['received'] = encode('0')
+            # server['message'] sends the message to the other user
             self.server['message'] = encode('')
 
+        # if ctrl+c is used to stop the execution of the program
         except KeyboardInterrupt:
             self.close_connection("Connection interrupted by keyboard. Closing connection to server", 1)
+        except urllib.error.URLError or urllib.error.HTTPError:
+            print("Cannot connect to server :( \nAre you connected to Eduroam/GlobalConnect VPN???")
+            sys.exit(1)
 
     def start_im_service(self):
         try:
             while self.check_connection():
+                # self.turn used to decide whether to listen for message or to receive message
                 if self.turn:
                     self.send_message()
                 else:
                     self.await_message()
             self.close_connection("Other user left the conversation. " +
                                   "Thank you for using the Messaging System for Healthcare Professionals.")
+        # if ctrl+c is used to stop the execution of the program
         except KeyboardInterrupt:
             self.close_connection("Connection interrupted by keyboard. Closing connection to server", 1)
 
@@ -84,11 +100,15 @@ class Server:
         myMessage = input("Enter a message to send to the other user: ")
 
         if self.check_connection():
+            # message validation
             if myMessage == '\n' or myMessage == '':
-                print(f'{colours.WARNING}Error. No message to send...{colours.ENDC}')
+                print(f'{colours.FAIL}Error. No message to send...{colours.ENDC}')
+                # by returning nothing, it will end the function which is then called again in start_im_service(),
+                # to then ask for the message again
                 return
             self.server['message'] = encode(myMessage)
             loading = ""
+            # to ensure other user has received the message before then listening for a message
             while decode(self.server['received']) == '0':
                 print(f"message sending{loading}", end='\r')
                 if loading.count('.') >= 10:
@@ -98,12 +118,18 @@ class Server:
             print("\nmessage sent")
             self.server['received'] = encode('0')
             self.next_turn()
+        else:
+            self.close_connection("Connection with user ended. Closing connection to server.")
 
     def await_message(self):
+        timeout = 0
         while decode(self.server['message']) == '':
             if self.check_connection():
                 print("Other user is typing", end='\r')
                 time.sleep(1)
+                timeout += 1
+                if timeout == 15:
+                    self.close_connection("Other user took too long to send a message. Connection to server is ended")
 
         if self.check_connection():
             print("You have received a message:\n" +
@@ -129,6 +155,7 @@ class Server:
 
         if decode(self.server['conn']) == 'closing':
             self.server.clear()
+            self.server['conn'] = encode('0')
             print('Closing connection with server')
             sys.exit()
 
---
+++ lab1/imclientv1.py
@@ -0,0 +1,92 @@
+import sys
+
+import im
+import time
+
+
+# problem with server['conn']
+class colors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def end_connection(message="connection to server ended."):
+    print(message)
+    if len(server.keys()) <= 2:
+        print("cleared")
+        server.clear()
+    else:
+        server.clear()
+        print("not clear")
+        server['conn'] = 'end'
+    sys.exit()
+
+
+def check_connection():
+    return server['user1'].decode().strip() == 'set' and server['user2'].decode().strip()
+
+
+try:
+    server = im.IMServerProxy('https://web.cs.manchester.ac.uk/t11915jr/COMP28112/lab1/IMserver.php')
+    # print(len(server.keys()))
+    if len(server.keys()) == 1:
+        server['user1'] = 'set'
+        server['turn'] = '1'
+        server['message'] = ''
+        my_id = 1
+    elif len(server.keys()) == 4:
+        server['user2'] = 'set'
+        my_id = 2
+    else:
+        print('Server is full. Try again later')
+        sys.exit()
+
+    my_turn = bool(my_id)
+
+    loading = ""
+    while len(server.keys()) == 4:
+        print(f'Searching for second client {loading}', end='\r')
+        if loading.count(".") <= 10:
+            loading += "."
+            time.sleep(1)
+        else:
+            print('timeout. Closing connection to server')
+            server.clear()
+            sys.exit()
+
+    print(f'connected. Welcome to the Messaging System for Healthcare Professionals. You are user {my_id}\n')
+    print('-------------------------------------------------------\n')
+    received_message = ''
+    while check_connection():
+        if str(my_id) in server['turn'].decode():
+            myMessage = input("\nEnter a message to send to the other user: ")
+            if myMessage == '\n' or myMessage == '':
+                print(f'{colors.WARNING}Error. No message to send...{colors.ENDC}')
+            elif not check_connection():
+                end_connection()
+            else:
+                server['message'] = myMessage.encode('UTF-8')
+                if my_id == 1:
+                    server['turn'] = '2'.encode('UTF-8')
+                else:
+                    server['turn'] = '1'.encode('UTF-8')
+                print(f'{colors.BOLD}message sent{colors.ENDC}\n')
+                time.sleep(0.5)
+        else:
+            print(f"{colors.UNDERLINE}Other user is typing...{colors.ENDC}", end='\r')
+            time.sleep(1)
+            if server['message'].decode() != '\n' and check_connection():
+                print(f"{colors.BLUE}You have received a message.\n" +
+                      f"{colors.CYAN}{server['message'].decode().strip()}{colors.ENDC}")
+                server['message'] = ''
+
+    end_connection()
+except KeyboardInterrupt:
+    end_connection()
